d58040932c24fd1d162e0aabe4d595d3b198f685
techtok7 <techtok7@gmail.com>
2020-05-27 11:46:29 +0530

    first commit

---
+++ Lavel_3/index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Notes</title>
+</head>
+<body>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP Notes</title>
+</head>
+<body>
+	<h1>Select Lavel</h1>
+	<a href='Lavel_1'>Lavel 1</a><br>
+	<a href='Lavel_2'>Lavel 2</a><br>
+	<a href='Lavel_3'>Lavel 3</a><br>
+</body>
+</html>
